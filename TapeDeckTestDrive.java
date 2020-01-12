6528e53dbfdfd6a6759083c787d7af472487b990
DopamineFlint <43389960+DopamineFlint@users.noreply.github.com>
2020-01-12 14:43:06 +0300

    Create TapeDeckTestDrive.java

---
+++ TapeDeckTestDrive.java
@@ -0,0 +1,25 @@
+class DrumKitTestDrive {
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		d.snare = false;
+		d.playSnare();
+		d.playTopHat();
+		
+		if (d.snare == true) {
+			d.playSnare();
+		}
+	}
+}
+
+class DrumKit {
+	boolean topHat = true;
+	boolean snare = true;
+	
+	void playSnare() {
+		System.out.println("Boom boom boom");
+	}
+	
+	void playTopHat() {
+		System.out.println("Ding ding ding");
+	}
+}
